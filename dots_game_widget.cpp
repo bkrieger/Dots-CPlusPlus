4bc36781627d47cfbd413eea05b17b49464df2f1
samanthamerritt <merritts@seas.upenn.edu>
2013-11-28 03:24:04 -0500

    worked on lines and dots and drawing

--- dots_game_widget.cpp
+++ dots_game_widget.cpp
@@ -4,12 +4,14 @@
 */
 
 #include <QtGui>
+#include <stdlib.h>
 #include "dots_game_widget.h"
 
 DotsGameWidget::DotsGameWidget(QWidget* parent)
   : QWidget(parent) // This passes the constructor parameter to the superclass
 {
   setFixedSize(WIDTH, HEIGHT);
+  //QWidget::setMouseTracking(true);
 }
 
 
@@ -17,11 +19,13 @@ void DotsGameWidget::reset()
 {
   for(int i = 0; i < 6; i++){
     for(int j = 0; j < 6; j++){
-        dot_board[i][j].color = Qt::red;
-        dot_board[i][j].x = i*BALL_RADIUS*5 + 2;
-        dot_board[i][j].y = j*BALL_RADIUS*5 + 2;        
+        dot_board[i][j].color = getDotColor();
+        dot_board[i][j].selected = false;
+        dot_board[i][j].x = i*BALL_RADIUS*2.5 + 5;
+        dot_board[i][j].y = j*BALL_RADIUS*2.5 + 5;        
     }
   }
+  line = NULL;
   // update() forces a repaint of the canvas
   update();
 }
@@ -56,9 +60,120 @@ void DotsGameWidget::paintEvent(QPaintEvent* event){
       p.drawEllipse(dot_board[i][j].x, dot_board[i][j].y, BALL_RADIUS * 2, BALL_RADIUS * 2);
     }
   }
+  //draw the lines in the LinkedList
+  if(line){
+    Line* curr = line;
+    while(curr != NULL){
+      p.setBrush(curr->color);
+      p.drawLine(curr->start_X, curr->start_Y, curr->end_X, curr->end_Y);
+      curr = curr->next;
+    }
+  }
 
 }
 
-void DotsGameWidget::mousePressEvent(QMouseEvent* event){}
-void DotsGameWidget::mouseReleaseEvent(QMouseEvent* event){}
-void DotsGameWidget::mouseMoveEvent(QMouseEvent* event){}
+void DotsGameWidget::mousePressEvent(QMouseEvent* event){
+  int x = event->x();
+  int y = event->y();
+  x = returnIndexOfDot(x);
+  y = returnIndexOfDot(y);
+
+  //set the current values
+  currentDotX = x;
+  currentDotY = y;
+  currentDotColor = dot_board[x][y].color;
+
+  //do we need selected? I figure, but haven't used it yet.
+  dot_board[x][y].selected = true;
+  //eventually take this out
+  dot_board[x][y].color = Qt::red;
+  line = NULL;
+
+  //force a repaint.
+  update();
+}
+void DotsGameWidget::mouseReleaseEvent(QMouseEvent* event){
+  //remove last line for visual purposes (TEMPORARY)
+  Line* curr = line;
+  while (curr->next->next != NULL){
+    curr = curr->next;
+  }
+  curr->next = NULL;
+  update();
+}
+
+void DotsGameWidget::mouseMoveEvent(QMouseEvent* event){
+  int x = event->x();
+  int y = event->y();
+
+  //index of the dot you are currently looking at
+  int dot_x = returnIndexOfDot(x);
+  int dot_y = returnIndexOfDot(y);
+  //write a function or test for adjacency to last looked at dot
+  bool is_adjacent = true;
+  //if the dot is of the same color
+  if(dot_board[dot_x][dot_y].color == currentDotColor){
+    //if the dot is adjacent
+    if(is_adjacent){
+      currentDotX = dot_x;
+      currentDotY = dot_y;
+
+      dot_board[dot_x][dot_y].selected = true;
+      dot_board[dot_x][dot_y].color = Qt::red;
+
+      //create a new line
+      Line* curr = line;
+      while(curr-> next != NULL){ curr = curr->next; }
+      curr->setEndPoint(currentDotX*(BALL_RADIUS*2 + BALL_RADIUS/2) + BALL_RADIUS + 5,
+        currentDotY*(BALL_RADIUS*2 + BALL_RADIUS/2) + BALL_RADIUS + 5);
+      curr->next = new Line(currentDotX*(BALL_RADIUS*2 + BALL_RADIUS/2) + BALL_RADIUS + 5, 
+        currentDotY*(BALL_RADIUS*2 + BALL_RADIUS/2) + BALL_RADIUS + 5, x, y, 
+        currentDotColor);
+    }
+  }
+
+  //Line for first dot
+  if(line){
+    Line* curr = line;
+    while(curr->next != NULL){ curr = curr->next; }
+    curr->setEndPoint(x, y);
+  } else{
+    line = new Line(currentDotX*(BALL_RADIUS*2 + BALL_RADIUS/2) + BALL_RADIUS + 5, 
+      currentDotY*(BALL_RADIUS*2 + BALL_RADIUS/2) + BALL_RADIUS + 5, x, y, 
+      currentDotColor);
+  }
+
+  update();
+}
+
+QColor DotsGameWidget::getDotColor(){
+  int choose = rand() % 4;
+  if(choose == 0) {
+    return Qt::white;
+  } else if (choose == 1) {
+    return Qt::darkCyan;
+  } else if (choose == 2) {
+    return Qt::darkMagenta;
+  } else {
+    return Qt::darkGray;
+  }
+}
+
+//freakishly hardcoding index selection.
+//fix when less tired.  at least a switch statement
+int DotsGameWidget::returnIndexOfDot(int pos){
+  int box_field = BALL_RADIUS*2+10; 
+  if (pos < box_field) {
+    return 0;
+  } else if (pos < 2*box_field) {
+    return 1;
+  } else if (pos < 3*box_field) {
+    return 2;
+  } else if (pos < 4*box_field) {
+    return 3;
+  } else if (pos < 5*box_field) {
+    return 4;
+  } else if (pos < 6*box_field) {
+    return 5;
+  }
+}
--- dots_game_widget.h
+++ dots_game_widget.h
@@ -6,6 +6,37 @@
 
 #include <QtGui>
 
+using namespace std;
+
+//LinkedList style class to draw lines
+class Line {
+public:
+  int start_X;
+  int start_Y;
+  int end_X;
+  int end_Y;
+  Line* next;
+  QColor color;
+
+  Line(int x, int y, int x2, int y2, QColor qcolor){
+    start_X = x;
+    start_Y = y;
+    end_X = x2;
+    end_Y = y2;
+    next = NULL;
+    color = qcolor;
+  }
+
+  void setEndPoint(int x, int y){
+    end_X = x;
+    end_Y = y;
+  }
+
+  void setNext(Line* nextLine){
+    next = nextLine;
+  }
+};
+
 class DotsGameWidget : public QWidget
 {
   Q_OBJECT
@@ -27,16 +58,23 @@ class DotsGameWidget : public QWidget
 
  private:
   bool isPaused;
-
-  static const int WIDTH = 400;
-  static const int HEIGHT = 400;
+  QColor getDotColor();
+  int returnIndexOfDot(int pos);
+  //these are the indices in the array of the current dot.
+  int currentDotX, currentDotY;
+  //track color for selection purposes
+  QColor currentDotColor;
+  static const int WIDTH = 375;
+  static const int HEIGHT = 375;
+  Line* line;
   struct dot 
   { 
     int x;
     int y;
     QColor color;
+    bool selected;
   } dot_board[6][6]; 
-  static const int BALL_RADIUS = 5;
+  static const int BALL_RADIUS = 25;
 };
 
 #endif
